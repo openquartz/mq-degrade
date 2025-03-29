a463c5918d933b000806931079f54e44e5ee080e
svnlab <openquartz@gmail.com>
2025-03-29 21:32:38 +0800

    feat()：初始化项目

---
+++ mq-degrade-send-spring-boot-starter/src/main/java/com/openquartz/mqdegrade/sender/common/Pair.java
@@ -0,0 +1,51 @@
+package com.openquartz.mqdegrade.sender.common;
+
+import java.util.Objects;
+
+public class Pair<K,V> {
+
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public static <K,V> Pair<K,V> of(K key, V value) {
+        return new Pair<>(key, value);
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) object;
+        return Objects.equals(key, pair.key) && Objects.equals(value, pair.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+}
---
+++ mq-degrade-send-spring-boot-starter/src/main/java/com/openquartz/mqdegrade/sender/core/impl/SendMessageFacadeImpl.java
@@ -0,0 +1,16 @@
+package com.openquartz.mqdegrade.sender.core.impl;
+
+import com.openquartz.mqdegrade.sender.core.SendMessageFacade;
+
+/**
+ * SendMessageFacadeImpl
+ *
+ * @author svnee
+ */
+public class SendMessageFacadeImpl implements SendMessageFacade {
+
+    public <T> boolean send(T message, String resource) {
+
+        return false;
+    }
+}

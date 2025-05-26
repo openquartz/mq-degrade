1a7606ac09d53ce81272605bc636ff565520a322
svnlab <xuzhaoah@163.com>
2025-05-26 15:23:00 +0800

    feat(): 优化MQ降级绑定配置代码

--- mq-degrade-send-spring-boot-starter/src/main/java/com/openquartz/mqdegrade/sender/core/factory/DegradeTransferBindingConfig.java
+++ mq-degrade-send-spring-boot-starter/src/main/java/com/openquartz/mqdegrade/sender/core/factory/DegradeTransferBindingConfig.java
@@ -2,8 +2,6 @@ package com.openquartz.mqdegrade.sender.core.factory;
 
 import com.openquartz.mqdegrade.sender.common.Pair;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
@@ -11,37 +9,45 @@ import java.util.function.Predicate;
 /**
  * 绑定降级传输配置
  *
+ * 使用样例：
+ * <code>
+ *      DegradeTransferBindingConfig
+ *                 .builder("test2")
+ *                 // 直接发送
+ *                 .send(String.class, messageProducer::sendMessage2)
+ *                 // 第一个消费分组降级传输去
+ *                 .degrade("test2_group1", String.class, msg -> {
+ *                     degradeMessageManager.degradeTransfer2(msg);
+ *                     return true;
+ *                 })
+ *                 // 第二个消费分组降级传输
+ *                 .degrade("test2_group2", String.class, msg -> {
+ *                     degradeMessageManager.degradeTransfer3(msg);
+ *                     return true;
+ *                 })
+ *                 .binding();
+ * </code>
+ *
  * @author svnee
  */
 public class DegradeTransferBindingConfig {
 
+    private DegradeTransferBindingConfig() {
+    }
+
     @SuppressWarnings("all")
-    public static class DegradeTransferBindingConfigBuilder {
+    public static class DegradeTransferBindingTransferConfigBuilder {
 
         private final String resource;
 
-        private Pair<Class, Predicate> sendPair;
+        private final Pair<Class, Predicate> sendPair;
+
+        private DegradeTransferBindingTransferConfigBuilder(String resource, Pair<Class, Predicate> sendPair) {
+            this.resource = resource;
+            this.sendPair = sendPair;
+        }
 
         private Map<String, Pair<Class, Predicate>> degradeList = new ConcurrentHashMap<>();
-
-        private DegradeTransferBindingConfigBuilder(String resource) {
-            this.resource = resource;
-        }
-
-        /**
-         * 直接发送配置
-         *
-         * @param messageClazz  消息类型
-         * @param sendPredicate 发送函数
-         * @param <T>           T 消息类
-         * @return builder
-         */
-        public <T> DegradeTransferBindingConfigBuilder send(Class<T> messageClazz, Predicate<T> sendPredicate) {
-            assert messageClazz != null;
-            assert sendPredicate != null;
-            this.sendPair = Pair.of(messageClazz, sendPredicate);
-            return this;
-        }
 
         /**
          * 降级传输配置
@@ -51,7 +57,7 @@ public class DegradeTransferBindingConfig {
          * @param <T>              T 降级传输消息类
          * @return builder
          */
-        public <T> DegradeTransferBindingConfigBuilder degrade(String degradeResource, Class<T> messageClazz, Predicate<T> degradePredicate) {
+        public <T> DegradeTransferBindingTransferConfigBuilder degrade(String degradeResource, Class<T> messageClazz, Predicate<T> degradePredicate) {
             assert messageClazz != null;
             assert degradePredicate != null;
             this.degradeList.putIfAbsent(degradeResource, Pair.of(messageClazz, degradePredicate));
@@ -72,7 +78,33 @@ public class DegradeTransferBindingConfig {
             degradeList.forEach((degradeResource, v) -> {
                 DegradeRouterFactory.register(resource, degradeResource, v.getKey(), v.getValue());
             });
+        }
+    }
 
+    @SuppressWarnings("all")
+    public static class DegradeTransferBindingConfigBuilder {
+
+        private final String resource;
+
+        private Map<String, Pair<Class, Predicate>> degradeList = new ConcurrentHashMap<>();
+
+        private DegradeTransferBindingConfigBuilder(String resource) {
+            this.resource = resource;
+        }
+
+        /**
+         * 直接发送配置
+         *
+         * @param messageClazz  消息类型
+         * @param sendPredicate 发送函数
+         * @param <T>           T 消息类
+         * @return builder
+         */
+        public <T> DegradeTransferBindingTransferConfigBuilder send(Class<T> messageClazz, Predicate<T> sendPredicate) {
+            assert messageClazz != null;
+            assert sendPredicate != null;
+            Pair<Class, Predicate> sendPair = Pair.of(messageClazz, sendPredicate);
+            return new DegradeTransferBindingTransferConfigBuilder(resource, sendPair);
         }
     }
 

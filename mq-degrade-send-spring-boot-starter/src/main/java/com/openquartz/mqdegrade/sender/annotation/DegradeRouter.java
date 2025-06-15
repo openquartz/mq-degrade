0aab69fce2d0923676fb34abb50518697c0d5c25
svnlab <xuzhaoah@163.com>
2025-06-15 15:38:27 +0800

    refactor(优化日志): 优化javadoc注释

--- mq-degrade-send-spring-boot-starter/src/main/java/com/openquartz/mqdegrade/sender/annotation/DegradeRouter.java
+++ mq-degrade-send-spring-boot-starter/src/main/java/com/openquartz/mqdegrade/sender/annotation/DegradeRouter.java
@@ -6,7 +6,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 发送路由
+ * 降级传输路由
+ *
+ * <p>使用样例：
+ * <pre>
+ *     &#064;DegradeRouter(resource = "SendTest",degradeResource = "test2_group1")
+ *      public boolean send(String msg) {
+ *          // TODO 发送消息
+ *          return true;
+ *      }
+ * </pre>
+ *</p>
  *
  * @author svnee
  */
